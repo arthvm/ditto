81017a456084f4864be9e96bcd3a0ba19d93a35e
Arthur Mariano <arthvm@proton.me>
2025-08-29 00:21:22 -0300

    chore: update commit command description and usage
    - Update the short description to reflect the new functionality: "Used to generated a git commit message from staged changes"
    - Update the long description to provide more details about the command's functionality:
      - Explain that the command uses the git-commit-message llm model by default to generate git commit messages for staged changes.
      - Indicate that the command will be able to change providers in the future to generate commit messages.
    
    These updates will help users understand the purpose and functionality of the commit command more clearly.

--- cmd/commit.go
+++ cmd/commit.go
@@ -13,13 +13,10 @@ import (
 
 var commitCmd = &cobra.Command{
 	Use:   "commit",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Used to generated a git commit message from staged changes",
+	Long: `Uses the git-commit-message llm model by default -
+	but will be able to change providers in the future - to
+	generate git commit messages for the stage changes`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		res, err := git.StagedDiff(cmd.Context())
 		if err != nil {
