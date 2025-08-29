bddacd5d0b15b2bad0bbd18af0c04d6085c06bbf
Arthur Mariano <arthvm@proton.me>
2025-08-28 23:59:27 -0300

    chore: clean up Cobra boilerplate and update app description
    - Remove generated comments and unused code in commit and root commands
    - Update root command description to reflect ditto's AI-powered git workflow purpose
    - Remove empty init function and unused flags from root.go
    - Clean up whitespace and formatting inconsistencies

--- cmd/commit.go
+++ cmd/commit.go
@@ -9,7 +9,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// commitCmd represents the commit command
 var commitCmd = &cobra.Command{
 	Use:   "commit",
 	Short: "A brief description of your command",
@@ -26,14 +25,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(commitCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// commitCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// commitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 Arthur Mariano
-
 */
 package cmd
 
@@ -10,42 +9,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ditto",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "An ai-application to simplify git workflows",
+	Long: `An AI application that allow devs to work faster by
+	delegating some of the tedious git related operations to an AI agent.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
 }
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ditto.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-
